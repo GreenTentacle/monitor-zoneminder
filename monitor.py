00cb885ce258baa4b57a76a523265e936ba8cd03
Nick <GreenTentacle@users.noreply.github.com>
2017-08-04 12:45:59 -0400

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,4 +1,22 @@
+#!/usr/bin/env python
+
+import requests
+import json
+
 # curl -XGET  http://server/zm/api/states.json # returns list of run states
 # curl -XPOST  http://server/zm/api/states/change/restart.json #restarts ZM
 # curl -XPOST  http://server/zm/api/states/change/stop.json #Stops ZM
 # curl -XPOST  http://server/zm/api/states/change/start.json #Starts ZM
+
+zmBaseURI = 'http://zoneminder.com/zm'
+zmAPIURI = '/'.join((zmBaseURI,'api'))
+zmCheckEndpoint = '/'.join((zmAPIURI,'states.json'))
+zmStartEndpoint = '/'.join((zmAPIURI,'states/change/start.json'))
+
+currentState = requests.get(zmCheckEndpoint)
+ 
+if currentState.status_code == 200:
+    data = json.loads(currentState.text)
+    print data['name']
+else:
+   print 'An error occurred querying the API'
