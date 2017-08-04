8d883f081f7ec835eb74556e5858addd81062525
Nick <GreenTentacle@users.noreply.github.com>
2017-08-04 12:30:31 -0400

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,4 @@
+# curl -XGET  http://server/zm/api/states.json # returns list of run states
+# curl -XPOST  http://server/zm/api/states/change/restart.json #restarts ZM
+# curl -XPOST  http://server/zm/api/states/change/stop.json #Stops ZM
+# curl -XPOST  http://server/zm/api/states/change/start.json #Starts ZM
